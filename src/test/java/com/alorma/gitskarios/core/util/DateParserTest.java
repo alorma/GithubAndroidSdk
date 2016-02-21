429034a7a5cc0ad8b1f38ec355f4670e9f4021d5
Roc Boronat <roc@fewlaps.com>
2016-02-21 19:51:43 +0100

    Moving the initialization of the DateParser to the @Before

--- src/test/java/com/alorma/gitskarios/core/util/DateParserTest.java
+++ src/test/java/com/alorma/gitskarios/core/util/DateParserTest.java
@@ -9,16 +9,20 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class DateParserTest {
 
+    DateParser dateParser = new DateParser();
+
+    @Before
+    public void init(){
+        dateParser = new DateParser();
+    }
+
     @Test(expected = NullPointerException.class)
     public void shouldLaunchANPE_whenPassingNull() throws Exception {
-        DateParser dateParser = new DateParser();
-
         dateParser.getMilisFromDateClearDay(null);
     }
 
     @Test
     public void shouldLaunchANPE_whenPassingBerniesValue() throws Exception {
-        DateParser dateParser = new DateParser();
         String gitHubDate = "2016-02-21T18:22:06Z";
 
         long result = dateParser.getMilisFromDateClearDay(gitHubDate);
